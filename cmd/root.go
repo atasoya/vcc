3451abad810317ef36f82be0b795393ca914d588
atasoya <ataatasoy2013@gmail.com>
2025-07-20 17:55:02 +0300

    bug: remove duplicate help text

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,7 @@ var rootCmd = &cobra.Command{
 	Use:   "vcc",
 	Short: "neoVim color Scheme Changer (vcc) is a CLI tool to streamline color scheme changing process for nvim.",
 	Long: `vcc is a CLI tool written in go to change color scheme in nvim with lazyvim.
-This CLI tool automates the nvim color scheme changing process.
-
-Usage:
-  vcc [command]
-
-Available Commands:
-  catppuccin	Sets color scheme to catppuccin mocha
-	
-Flags:
-  -v --variant string	variant of selected color scheme
-	
-Use "vcc [command] --help for more information about a command!"`,
+This CLI tool automates the nvim color scheme changing process.`,
 }
 
 func Execute() {
